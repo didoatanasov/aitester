8b4948f3ce6d53e87f56c95e81d8120607dcd5bb
dido <dido@omisoft.eu>
2019-08-22 15:05:05 +0300

    Fixes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private camera: Camera) { }
 
+
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    console.log('tale photo 1 ');
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log('Picture taken:' + imageData);
+    }, (err) => {
+      console.error('ERROR TAKING PICTURE');
+      // Handle error
+    });
+  }
 }
+
+
+
